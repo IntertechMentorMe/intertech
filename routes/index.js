bb4258b6b37b90db200ab990d9a14e277bc07782
Oliver Jones <olli@Olivers-MacBook-Air.local>
2015-12-12 23:46:41 +0000

    Started updating all the pages

--- routes/index.js
+++ routes/index.js
@@ -8,14 +8,4 @@ var controller = function(req, res) {
 
 router.get('/', controller);
 
-res.render('user', {
-  helpers: {
-    firstName: function() { return passport.first_name; },
-    lastName: function() { return passport.last_name; },
-    headline: function() { return passport.headline; },
-    summary: function() { return passport.summary; },
-    photo: function() { return passport.photo; }
-  }
-});
-
 module.exports = router;
