9808d7ffd4bfaf46ff4cc3d5ca1214c0888b5721
Oliver Jones <olli@Olivers-MacBook-Air.local>
2015-12-13 08:46:37 +0000

    Added terms and conditions

--- routes/index.js
+++ routes/index.js
@@ -3,9 +3,24 @@ var router = express.Router();
 
 var controller = function(req, res) {
   var hidden = req.query.action=="signup"? "" : "hidden";
-  res.render("index", {showTerms: hidden, end: req.query.end});
+  var end = req.query.end;
+  res.render("index", {showTerms: hidden, end: req.query.end, mentee: end=="" || end=="mentee", mentor: end=="" || end=="mentor"});
 };
 
 router.get('/', controller);
+
+router.get('/mentor', function(req, res) {
+  var session = req.session;
+  session.isMentor = true;
+  req.session.save();
+  res.redirect('/auth/login');
+});
+
+router.get('/mentee', function(req, res) {
+  var session = req.session;
+  session.isMentor = false;
+  req.session.save();
+  res.redirect('/auth/login');
+});
 
 module.exports = router;
